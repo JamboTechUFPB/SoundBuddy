3effb4fca170e53ab160fcedc9a8cfe4afbf39f1
erlonL <erlon.avelino@academico.ufpb.br>
2025-04-13 21:38:44 -0300

    chore: removed src/ from backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
